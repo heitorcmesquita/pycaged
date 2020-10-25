64570cf3cd5f424aef04b893956cbf0f9b47fa8d
heitorcmesquita <42346500+heitorcmesquita@users.noreply.github.com>
2020-10-25 18:04:35 -0300

    converter para rst

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name = 'pycaged',
     version = '0.2',
     description = 'fetches CAGED microdata / busca microdados do CAGED',
-    long_description = open('README.md').read(),
+    long_description = open('README.rst').read(),
     url = '',
     author = 'Heitor Caixeta',
     author_email = 'heitor.ca.mesquita@gmail.com',

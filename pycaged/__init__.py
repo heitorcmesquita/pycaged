808b74652a7a8e38b3a5f63961b69a0564391187
heitorcmesquita <42346500+heitorcmesquita@users.noreply.github.com>
2020-10-26 16:01:14 -0300

    Update __init__.py

--- pycaged/__init__.py
+++ pycaged/__init__.py
@@ -84,7 +84,6 @@ def getCagedState(ano, mes, uf):
         print('microdados lidos, fazendo cálculos')
             
         #Contando número de contratações e demissões em um DF separado. Fazendo a média salárial do agrupamento 
-        data = data[(data.uf == uf)].reset_index()
         data['região'] = '2020'
         data['Count'] = ''
         data.rename(columns = {"saldomovimentação":"Admitidos/Desligados", "região":"Ano Declarado", "seção":"Seção", 'sexo':'Sexo', 'salário':'Salário Mensal'}, inplace = True) 
@@ -178,7 +177,6 @@ def getCagedMun(ano, mes, uf):
         print('microdados lidos, fazendo cálculos')
             
         #Contando número de contratações e demissões em um DF separado. Fazendo a média salárial do agrupamento 
-        data = data[(data.uf == uf)].reset_index()
         data['região'] = '2020'
         data['Count'] = ''
         data.rename(columns = {"município":"Município", "saldomovimentação":"Admitidos/Desligados", "região":"Ano Declarado", "seção":"Seção", 'sexo':'Sexo', 'salário':'Salário Mensal'}, inplace = True) 
@@ -197,5 +195,94 @@ def getCagedMun(ano, mes, uf):
         print('Os dados do mês ' + mes + ' de ' + year + ' foram retornados com sucesso')
         return data
     
+    
+   def getCagedBr(ano, mes):
+    
+    ''' ano = int; mes = str
+    Retorna um dataframe com um resumo dos microdados do CAGED para o mês e estado selecionados
+    
+    O README deste pacote mostra um código para uma iteração completa destes dados.
+    Acesse em https://github.com/heitorcmesquita/pycaged
+    '''
+    year = str(ano)
+    
+    if ano <2020: 
+       #Baixando e extraindo microdados
+        url = "ftp://ftp.mtps.gov.br/pdet/microdados/CAGED/"+year+"/CAGEDEST_"+mes+year+".7z"
+        wget.download(url, 'caged.7z')
+        archive = SevenZipFile('caged.7z', mode = 'r')
+        archive.extractall()
+        print('microdados baixados com sucesso, passando para leitura')
+        
+        #Lendo o arquivo txt (tem alguns sem '_'). Depois, deletando arquivos
+        try:
+            data = pd.read_csv('CAGEDEST_'+mes+year+'.txt', sep = ';', encoding = 'iso-8859-1')
+        except:
+            data = pd.read_csv('CAGEDEST'+mes+year+'.txt', sep = ';', encoding = 'iso-8859-1');
+        archive.close()
+        remove('caged.7z')
+        try:
+            remove('CAGEDEST_'+mes+year+'.txt')
+        except:            
+            remove('CAGEDEST'+mes+year+'.txt')  
+        print('microdados lidos, fazendo cálculos')
+            
+        #Unindo dados de classificação do IBGE, preenchendo valores da seçao e tratando dados de salário
+        data = pd.merge(data, cnae, left_on = 'CNAE 2.0 Classe', right_on = 'Classe', how = 'left')
+        data['Seção'].fillna('NA', inplace = True)
+        data['Salário Mensal'] = [d.replace(',','.') for d in data['Salário Mensal']]
+        data['Salário Mensal'] = pd.to_numeric(data['Salário Mensal'])
+        
+        #Contando número de contratações e demissões em um DF separado. Fazendo a média salárial do agrupamento 
+        data['Count'] = ''
+        data2 = data.groupby(['Admitidos/Desligados', 'Ano Declarado', 'Seção', 'Sexo', 'UF'], as_index = False)['Count'].count()   
+        data = data.groupby(['Admitidos/Desligados', 'Ano Declarado', 'Seção', 'Sexo', 'UF'], as_index = False)['Salário Mensal'].mean()           
+        data['Count'] = '';
+        data['mes'] = mes
+        
+        #Trazendo dados de quantidade de contratações e demissões, e retornando DF    
+        for k in range(0,len(data2)):
+            data['Count'][k] = data2['Count'][k] 
+        
+        data['Admitidos/Desligados'] = data['Admitidos/Desligados'].replace(2,-1)
+        data['Salário Mensal'] = data['Salário Mensal'].astype(int)
+        print('Os dados do mês ' + mes + ' de ' + year + ' foram retornados com sucesso')
+        return data
+        
+    elif ano == 2020:
+        #Baixando e extraindo microdados
+        url = 'ftp://ftp.mtps.gov.br/pdet/microdados/NOVO CAGED/Movimentações/2020/'+dicmes[mes]+'/CAGEDMOV2020'+mes+'.7z'
+        try:
+            wget.download(url, 'caged.7z')
+        except:
+            print('Os microdados do mês selecionado ainda não estão disponíveis')   
+        archive = SevenZipFile('caged.7z', mode = 'r')
+        archive.extractall()
+        print('microdados baixados com sucesso, passando para leitura')
+        
+       #Lendo o arquivo txt e filtrando por UF. Depois, deletando arquivos baixados
+        data = pd.read_csv('CAGEDMOV2020'+mes+'.txt', sep = ';', encoding = 'UTF-8')
+        archive.close()
+        remove('caged.7z')
+        remove('CAGEDMOV2020'+mes+'.txt')  
+        print('microdados lidos, fazendo cálculos')
+            
+        #Contando número de contratações e demissões em um DF separado. Fazendo a média salárial do agrupamento 
+        data['região'] = '2020'
+        data['Count'] = ''
+        data.rename(columns = {"saldomovimentação":"Admitidos/Desligados", "região":"Ano Declarado", "seção":"Seção", 'sexo':'Sexo', 'salário':'Salário Mensal', 'uf':'UF'}, inplace = True) 
+        data2 = data.groupby(['Admitidos/Desligados', 'Ano Declarado', 'Seção', 'Sexo', 'UF'], as_index = False)['Count'].count()   
+        data = data.groupby(['Admitidos/Desligados', 'Ano Declarado', 'Seção', 'Sexo', 'UF'], as_index = False)['Salário Mensal'].mean()           
+        data['Count'] = ''
+        data['mes'] = mes
+        
+        #Trazendo dados de quantidade de contratações e demissões, e retornando DF    
+        for k in range(0,len(data2)):
+            data['Count'][k] = data2['Count'][k] 
+        
+        data['Sexo'] = data['Sexo'].replace(3,2)
+        data['Salário Mensal'] = data['Salário Mensal'].astype(int)
+        print('Os dados do mês ' + mes + ' de ' + year + ' foram retornados com sucesso')
+        return data 
 
 

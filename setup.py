28392ba3c7eb5fcffb6a0b163e80b512df8f84cb
heitorcmesquita <42346500+heitorcmesquita@users.noreply.github.com>
2020-11-06 16:48:40 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ classifiers = ['Operating System :: Microsoft :: Windows :: Windows 10',
 #Calling setup
 setup(
     name = 'pycaged',
-    version = '0.7.2',
+    version = '0.7.3',
     description = 'fetches CAGED microdata / busca microdados do CAGED',
     long_description=long_description,
     long_description_content_type='text/markdown',
